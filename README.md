671a9e969be2a31a903c1b5eab9c7f5741416780
Kevin Jessie <kevinjessie32@yahoo.com>
2023-06-07 20:38:01 -0700

    update reamdme

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # SVG-Logo-Maker
+
+## Description
+To build a Node.js command-line application that takes in user input to generate a logo and save it as an SVG file Links to an external site.. The application prompts the user to select a color and shape, provide text for the logo, and save the generated SVG to a .svg file.
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+To install necessary dependencies, run the following command:
+\`\`\`
+npm install
+\`\`\`
+
+## Usage
+Run the following command at the root of your project and answer the prompted questions:
+\`\`\`
+node index.js
+\`\`\`
+
+## License
+This project is licensed under the MIT license.
+
+## Tests
+To run tests, run the following command:
+
+\`\`\`
+npm test
+\`\`\`
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](
+
--- index.js
+++ index.js
@@ -1,52 +1,62 @@
 // inquirer imports
 const inquirer = require('inquirer');
-// Import the classes
-const { Triangle, Circle, Square } = require('../lib/shapes');
-// Import the classes
-const { Triangle, Circle, Square } = require('../lib');
+// import shapes
+const { Triangle, Circle, Square } = require('../lib/shapes.js');
 // Create a list of choices for inquirer
 const choices = ['Triangle', 'Circle', 'Square'];
- const fs = require('fs');
 // Prompt the user for a shape
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'What shape would you like to create?',
-            choices: choices
-        }
-    ])
-    .then(({ shape }) => {
-        // Prompt the user for the shape's dimensions
-        inquirer
-            .prompt([
-                {
-                    type: 'input',
-                    name: 'dimensions',
-                    message: `Enter the dimensions of the ${shape} (comma separated):`
-                }
-            ])
-            .then(({ dimensions }) => {
-                // Create a new instance of the selected shape
-                let shapeObj;
-                switch (shape) {
-                    case 'Triangle':
-                        const [height, base] = dimensions.split(',');
-                        shapeObj = new Triangle(height, base);
-                        break;
-                    case 'Circle':
-                        const [radius] = dimensions.split(',');
-                        shapeObj = new Circle(radius);
-                        break;
-                    case 'Square':
-                        const [side] = dimensions.split(',');
-                        shapeObj = new Square(side);
-                        break;
-                }
-                // Print the area of the shape
-                console.log(`The area of the ${shape} is ${shapeObj.area()}`);
-            });
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like to calculate the area of?',
+        choices: choices
     }
-    );
+]).then((answers) => {
+    // Prompt the user for the shape's dimensions
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'height',
+            message: 'What is the height of the shape?',
+            when: () => answers.shape === 'Triangle'
+        },
+        {
+            type: 'input',
+            name: 'base',
+            message: 'What is the base of the shape?',
+            when: () => answers.shape === 'Triangle'
+        },
+        {
+            type: 'input',
+            name: 'radius',
+            message: 'What is the radius of the shape?',
+            when: () => answers.shape === 'Circle'
+        },
+        {
+            type: 'input',
+            name: 'side',
+            message: 'What is the side length of the shape?',
+            when: () => answers.shape === 'Square'
+        }
+    ]).then((answers) => {
+        // Create the correct shape object
+        let shape;
+        switch (answers.shape) {
+            case 'Triangle':
+                shape = new Triangle(answers.height, answers.base);
+                break;
+            case 'Circle':
+                shape = new Circle(answers.radius);
+                break;
+            case 'Square':
+                shape = new Square(answers.side);
+                break;
+        }
+        // Print the area
+        console.log(`The area is ${shape.area()}`);
+    });
+}
+);
+
 
