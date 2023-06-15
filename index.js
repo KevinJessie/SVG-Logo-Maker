8f3128d639070dbb61af2f1b06aec05627a0bc7f
Kevin Jessie <kevinjessie32@yahoo.com>
2023-06-15 16:57:29 -0700

    updating app

--- index.js
+++ index.js
@@ -1,72 +1,62 @@
-// Desc: Main entry point for the application
-
-// Import dependencies
 const inquirer = require('inquirer');
+const { Shape, Triangle, Square, Circle } = require('./lib/shapes');
 const fs = require('fs');
-const generateLogo = require('./utils/generateLogo.js');
-const shape = require('shape.js');
 
-// Prompt user for input
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'Shape',
-            message: 'What shape would you like to use?',
-            choices: ['Triangle', 'Square', 'Circle']
-        },
-        {
-            type: 'input',
-            name: 'Color',
-            message: 'What color would you like to use?'
+const questions = [
+    {
+        type: 'input',
+        name: 'shape',
+        message: 'Choose shape would you like to create?',
+        answers: ['Triangle', 'Square', 'Circle']
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'What color would you like your shape to be?'
 
-        },
-        {
-            type: 'input',
-            name: 'text',
-            message: 'What text would you like to use(3letters)?'
-
-        },
-    ]);
-
-// fuction to write  
-function writeToFile(fileName, data) {
-    var fileName = "logo.svg";
-    fs.writeFile(fileName, data, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("Generating logo.svg");
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What text would you like your shape to display?'
+        
     }
-    )
-}
+];
 
-// function to initialize program
-function init() {
-    inquirer.prompt(questions).then((answers) => {
-        console.log(answers);
-        writeToFile("logo.svg", generateLogo(answers));
-    });
-}
-
-// function call to initialize program
-init();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+inquirer.prompt(questions).then(
+    ({ shape, color, text }) => {
+        let svgElement;
+        switch (shape) {
+            case 'Triangle':
+                const triangle = new Triangle();
+                triangle.setColor(color);
+                svgElement = triangle.render();
+                break;
+            case 'Square':
+                const square = new Square();
+                square.setColor(color);
+                svgElement = square.render();
+                break;
+            case 'Circle':
+                const circle = new Circle();
+                circle.setColor(color);
+                svgElement = circle.render();
+                break;
+            default:
+                break;
+        }
+        const x = 150 - (text.length * 5);
+        const y = 100;
 
 
-
-
+        const svg = `<svg xmls= "http://www.w3.org/2000/svg" width="300" height="200">
+        ${svgElement}
+        <text x="${x}" y="${y}" fill="black">${text}</text>
+        </svg>`;
+        fs.writeFile('logo.svg', svg, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        }
+        );
+    } 
+);
